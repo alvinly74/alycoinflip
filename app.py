fdcc7f49efa2a3cea174f3cb84de89b34366e578
Alvin Ly <alvinly74@gmail.com>
2016-08-19 21:06:14 -0700

    yolo get it working

--- app.py
+++ app.py
@@ -34,12 +34,11 @@ def webook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
                     # ['entry'][0]['messaging'][0]['message']['text']
-                    send_message(sender_id, messaging_event.get('text'))
+                    send_message(sender_id, message_text)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
