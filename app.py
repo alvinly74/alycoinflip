34a140c81660c1268063ff38183b66c6c572df28
Alvin Ly <alvinly74@gmail.com>
2016-08-19 23:50:24 -0700

    attempt split totals

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def webook():
                         tax_value = float(message_text.split()[2:3][0])
                         costs = message_text.split()[3:-1]
                         costs = map(float, costs)
-                        tax_percentages = map(lambda price: price/total, costs)
                         total = sum(costs)
+                        tax_percentages = map(lambda price: price/total, costs)
                         grand_totals = []
                         
                         for idx in range(len(costs)):
